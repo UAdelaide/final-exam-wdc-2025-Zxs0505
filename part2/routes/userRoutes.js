5ff27fc21fba4712723ae2626e58e3cae691328b
Zxs0505 <15238078045@163.com>
2025-06-20 07:10:00 +0000

    [automatic commit] 2025-06-20 07:10:00 UTC 180.150.112.7

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -70,5 +70,16 @@ router.post('/login', async (req, res) => {
 });
 
 
+router.post('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      return res.status(500).json({ error: 'Logout failed' });
+    }
+    res.clearCookie('connect.sid'); 
+    res.json({ message: 'Logged out successfully' });
+  });
+});
+
+
 
 module.exports = router;
