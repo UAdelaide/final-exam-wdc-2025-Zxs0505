39a9217604eaec5dd46b43dfcc366911ef4fc898
Zxs0505 <15238078045@163.com>
2025-06-20 02:25:54 +0000

    [automatic commit] 2025-06-20 02:25:54 UTC 49.178.97.61

--- starthere/app.js
+++ starthere/app.js
@@ -30,7 +30,7 @@ let db;
     db = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
-      password: 'Nishi12345678',
+      password: '',
       database: 'DogWalkService'
     });
 
