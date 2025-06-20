6c46cbc3ab6c304e9637a5ecae61ea89f5be07f0
Zxs0505 <15238078045@163.com>
2025-06-20 08:12:43 +0000

    [automatic commit] 2025-06-20 08:12:42 UTC 180.150.112.7

--- part2/app.js
+++ part2/app.js
@@ -10,7 +10,7 @@ const walkRoutes = require('./routes/walkRoutes');
 const app = express();
 const PORT = 3000;
 
-app.use(cors({origin: 'http://localhost:3000', credentials: true}));
+app.use(cors({}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
